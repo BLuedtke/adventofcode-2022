f3aad24350e159938900909bff45a40b827c5e7c
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2022-12-05 22:29:18 +0100

    initial, started with day5

---
+++ src_tmpl/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Bernhard Luedtke
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-05
+ * 
+*/
+
+
+#include <ios>
+#include <iostream>
+#include <iomanip>
+#include <numbers>
+#include <chrono>
+#include <thread>
+
+#include <benchmark/benchmark.h>
+#include <vector>
+#include <string>
+#include <array>
+#include <tuple>
+
+using std::cout; //NOLINT
+using std::get; //NOLINT
+using namespace std::chrono;
+using namespace std::chrono_literals;
+constexpr size_t targetSize = 100000;
+
+int main(int argc, char **argv){
+	return 0;
+}
